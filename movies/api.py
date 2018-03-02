5e8883461e25734287a55890561bb07cc6eb3eff
jaime <jaime@kasfactory.net>
2018-03-02 10:21:22 +0100

    changes in movies-api and added requirements.txt

--- movies/api.py
+++ movies/api.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from movies.models import Movie
 from movies.serializers import MovieSerializer
-
+from rest_framework import status
 
 class MovieListAPI(APIView):
 
@@ -10,3 +10,15 @@ class MovieListAPI(APIView):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies,many=True)
         return Response(serializer.data)
+
+
+    def post(self,request):
+        serializer = MovieSerializer(data=request.POST)
+        if serializer.is_valid():
+            new_movie = serializer.save()
+            return Response(serializer.data,status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors,status.HTTP_400_BAD_REQUEST)
+
+    def update(self,instance,validated_data):
+        ''
